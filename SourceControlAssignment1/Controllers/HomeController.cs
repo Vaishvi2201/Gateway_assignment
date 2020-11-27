869ac42d8e33cd50635020d6d22869292904fde0
vaishvi <vaishvipatel365@gmail.com>
2020-11-27 17:36:51 +0530

    Source control assignment 1 done

---
+++ SourceControlAssignment1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SourceControlAssignment1.Models;
+
+namespace SourceControlAssignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Employee e)
+        {
+            if (ModelState.IsValid == true)
+            {
+                ViewData["SuccessMessage"] ="<script>alert('Data has been successfully added')</script>";
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ SourceControlAssignment1/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SourceControlAssignment1.Models
+{
+    public class Employee
+    {
+        [Required (ErrorMessage ="Please enter your ID")] [StringLength(2)]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Name")]
+        [MaxLength(12)][MinLength(3)]
+        public string EmployeeName { get; set; }
+        
+
+        [Required(ErrorMessage = "Please enter your Age")]
+        [Range(18,25)]
+        public int EmployeeAge { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your Email")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string EmployeeEmail { get; set; }
+        
+
+        [Required(ErrorMessage = "Please enter your Gender")]
+        public string EmployeeGender { get; set; }
+
+    }
+}
