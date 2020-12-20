f4d6c537ccf26d9332d93463ea43f94b194cdb71
vaishvi <vaishvipatel365@gmail.com>
2020-12-20 18:40:39 +0530

    Source Control Final Assignmnet Submission

---
+++ SourceControlFinalAssignment/Models/DB_Entities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+//using SourceControlFinalAssignment.Models;
+
+
+namespace SourceControlFinalAssignment.Models
+{
+    public class DB_Entities : DbContext
+    {
+        public DB_Entities() : base("name=MyDatabase") { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
---
+++ SourceControlFinalAssignment/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SourceControlFinalAssignment.Models
+{
+    public class User
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Required]
+       
+        public string Email { get; set; }
+
+
+       /** [Required]
+        [FileExtensions(Extensions ="png,jpg",ErrorMessage ="Please upload png and jpg images")]
+        public HttpPostedFileBase Photogaph { get; set; }**/
+        
+        
+        [Required]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        public string FullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
